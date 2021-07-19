8247c01f3cadaaa2ad06ce3392a322d1c07916a2
Pranik Chainani <pranikc@gmail.com>
2021-07-18 22:39:23 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+
+        "My name is Pranik"
+      
+      end
+
+
+    get '/hometown' do
+
+        "My hometown is Houston"
+  
+    end
+
+    get '/favorite-song' do
+
+        "My favorite song is Happiness is a Warm Gun"
+  
+    end
 end
